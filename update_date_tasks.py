198fc25595f0167d2bde5a5e39266a7d55f927a1
luis.filipe <luis.filipe@newm.com.br>
2024-05-24 11:11:01 -0300

    Alteração para Criação e Atualização em Lote

--- update_date_tasks.py
+++ update_date_tasks.py
@@ -7,6 +7,13 @@ from domain.item_update import ItemUpdate
 from services.todoist_services import TodoistServices
 
 
+def is_not_expire_task(due_date_task: datetime) -> bool:
+    if due_date_task > datetime.now() + timedelta(days=1):
+        return True
+
+    return False
+
+
 def main():
     load_dotenv()
     todoist_project = os.getenv('TODOIST_PROJECT_UPDATED_DATE')
@@ -24,7 +31,7 @@ def main():
         if item.project_id in projects and item.due_date is not None:
             due_date_task = datetime.fromisoformat(item.due_date.split('T')[0])
 
-            if due_date_task > datetime.now() + timedelta(days=1):
+            if is_not_expire_task(due_date_task):
                 continue
 
             due_date = (due_date_task + timedelta(days=90)).replace(hour=15, minute=0, second=0).isoformat()
